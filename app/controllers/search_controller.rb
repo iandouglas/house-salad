d04a43bbac4c3b54afe9feaefec92f4b3a6db2d2
Dione Wilson <dionew1@gmail.com>
2020-08-18 10:52:55 -0600

    Complete propublica connection in search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,12 @@
 class SearchController < ApplicationController
   def index
+    state = params[:state]
+
+    conn = Faraday.new(url: 'https://api.propublica.org') do |f|
+      f.headers['X-API-Key] = ENV['PROPUBLICA_KEY']
+    end
+
+    response = conn.get("/congress/v1/members/house/#{state}/current.json")
+    binding.pry
   end
 end

4e6a11f854a7017f5b6dd8f8f1f196e78dcd9ae2
Dione Wilson <dionew1@gmail.com>
2020-08-21 14:31:08 -0600

    Complete passing feature test, ready for refactor

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,12 @@ class SearchController < ApplicationController
     state = params[:state]
 
     conn = Faraday.new(url: 'https://api.propublica.org') do |f|
-      f.headers['X-API-Key] = ENV['PROPUBLICA_KEY']
+      f.headers['X-API-Key'] = ENV['PROPUBLICA_KEY']
     end
 
     response = conn.get("/congress/v1/members/house/#{state}/current.json")
-    binding.pry
+
+    members_info = JSON.parse(response.body, symbolize_names: true)
+    @members = members_info[:results]
   end
 end
